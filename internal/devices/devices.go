4c8a87c38d36ad61fcd0f6ac50f4b3e4ef558fac
dm4brl <92757328+dm4brl@users.noreply.github.com>
2025-03-30 18:54:35 -0300

    Create devices.go

---
+++ internal/devices/devices.go
@@ -0,0 +1,34 @@
+package devices
+
+import (
+    "fmt"
+    "time"
+    "math/rand"
+    "switch-emulator/internal/mqtt"
+    "switch-emulator/internal/redis"
+    "switch-emulator/internal/kafka"
+    "switch-emulator/internal/db"
+)
+
+// Эмуляция устройства, которое периодически меняет свое состояние
+func SimulateDevice(deviceID string) {
+    states := []string{"ON", "OFF"}
+    for {
+        state := states[rand.Intn(len(states))] // случайное состояние
+
+        // Публикуем в MQTT
+        mqtt.Publish("home/lighting/" + deviceID + "/status", state)
+
+        // Сохраняем состояние в Redis
+        redis.SetDeviceState(deviceID, state)
+
+        // Отправляем сообщение в Kafka
+        kafka.SendMessage(kafka.Connect("localhost:9092", "device-events"), fmt.Sprintf("Device %s state: %s", deviceID, state))
+
+        // Сохраняем в базе данных
+        db.SaveDeviceState(deviceID, state)
+
+        // Задержка в 15 минут
+        time.Sleep(15 * time.Minute)
+    }
+}
